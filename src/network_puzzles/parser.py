eb815b6aed779208d192521fd3870c8f6b8450e3
Nate Marti <nate_marti@sil.org>
2025-05-20 16:12:28 +0100

    small tweaks; especially catch specific Exception if pattern can't be converted to int

--- src/network_puzzles/parser.py
+++ src/network_puzzles/parser.py
@@ -9,44 +9,46 @@ from . import puzzle
 def parse(command:str):
     #We will make this a lot more interesting later.  For now, just do a very simple thing
     #try:
-        val=None
-        cmd=""
-        items = command.split() #break at all whitespace
+        val = None
+        cmd = ""
+        items = command.split() # break at all whitespace
         if len(items) > 0:
-            #we have stuff.  Process it
-            cmd=items[0].lower()
-            if items[0].lower() in ['list','show','search']:
+            # we have stuff. Process it
+            cmd = items[0].lower()
+            if cmd in ['list','show','search']:
                 #We want to list all the items.
-                pattern=None
-                if(len(items) > 1):
-                    pattern=items[1]
+                pattern = None
+                if len(items) > 1:
+                    pattern = items[1]
                     try:
-                        #If it is just a number, look for that level
-                        pattern="Level"+str(int(pattern))
-                    except:
-                        #otherwise, leave the pattern alone
-                        pattern=pattern
-                    if not re.search(r"^\.\*",pattern):
-                        pattern=r".*"+pattern
-                    if not re.search(r"\.\*$",pattern):
-                        pattern=pattern+r".*"
-                list=puzzle.listPuzzles(pattern)
-                val=list
-                for a in list:
+                        # If it is just a number, look for that level. int(pattern)
+                        # throws a ValueError Exception if it fails, so we catch
+                        # that specific exception below.
+                        pattern = "Level" + str(int(pattern))
+                    except ValueError:
+                        # otherwise, leave the pattern alone
+                        pass
+                    if not re.search(r"^\.\*" , pattern):
+                        pattern = r".*" + pattern
+                    if not re.search(r"\.\*$", pattern):
+                        pattern = pattern + r".*"
+                pzs = puzzle.listPuzzles(pattern)
+                val = pzs
+                for a in pzs:
                      print(a)
-                return { 'command':cmd, 'value':val }
-            elif items[0].lower() in ['load', 'open']:
+                return {'command': cmd, 'value': val}
+            elif cmd in ['load', 'open']:
                 if len(items) == 2:
-                    val=puzzle.choosePuzzle(items[1])
+                    val = puzzle.choosePuzzle(items[1])
                 elif len(items) == 3:
-                    val=puzzle.choosePuzzle(items[1],items[2])
+                    val = puzzle.choosePuzzle(items[1], items[2])
                 else:
                     print("loading: ")
-                return { 'command':cmd, 'value':val }
-            elif items[0].lower() in ['exit', 'quit', 'stop']:
+                return {'command':cmd, 'value':val}
+            elif cmd in ['exit', 'quit', 'stop']:
                  sys.exit()
             else:
-                print("unknown: " + command)
+                print(f"unknown: {command}")
     #except:
         #We will have a better process later.  Right now, do this simply
         #print("something went wrong parsing.")
