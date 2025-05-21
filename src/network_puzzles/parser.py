b8cf61d9c47ade969d86e765d91ddf6fd8a4713c
Tim Young <Tim.Young@LightSys.org>
2025-05-21 14:31:01 -0500

    Changed the parser to use match case statements instead of if/else

--- src/network_puzzles/parser.py
+++ src/network_puzzles/parser.py
@@ -15,40 +15,41 @@ def parse(command:str):
         if len(items) > 0:
             # we have stuff. Process it
             cmd = items[0].lower()
-            if cmd in ['list','show','search']:
-                #We want to list all the items.
-                pattern = None
-                if len(items) > 1:
-                    pattern = items[1]
-                    try:
-                        # If it is just a number, look for that level. int(pattern)
-                        # throws a ValueError Exception if it fails, so we catch
-                        # that specific exception below.
-                        pattern = "Level" + str(int(pattern))
-                    except ValueError:
-                        # otherwise, leave the pattern alone
-                        pass
-                    if not re.search(r"^\.\*" , pattern):
-                        pattern = r".*" + pattern
-                    if not re.search(r"\.\*$", pattern):
-                        pattern = pattern + r".*"
-                pzs = puzzle.listPuzzles(pattern)
-                val = pzs
-                for a in pzs:
-                     print(a)
-                return {'command': cmd, 'value': val}
-            elif cmd in ['load', 'open']:
-                if len(items) == 2:
-                    val = puzzle.choosePuzzle(items[1])
-                elif len(items) == 3:
-                    val = puzzle.choosePuzzle(items[1], items[2])
-                else:
-                    print("loading: ")
-                return {'command':cmd, 'value':val}
-            elif cmd in ['exit', 'quit', 'stop']:
-                 sys.exit()
-            else:
-                print(f"unknown: {command}")
+            match cmd:
+                case 'list'|'show'|'search':
+                    #We want to list all the items.
+                    pattern = None
+                    if len(items) > 1:
+                        pattern = items[1]
+                        try:
+                            # If it is just a number, look for that level. int(pattern)
+                            # throws a ValueError Exception if it fails, so we catch
+                            # that specific exception below.
+                            pattern = "Level" + str(int(pattern))
+                        except ValueError:
+                            # otherwise, leave the pattern alone
+                            pass
+                        if not re.search(r"^\.\*" , pattern):
+                            pattern = r".*" + pattern
+                        if not re.search(r"\.\*$", pattern):
+                            pattern = pattern + r".*"
+                    pzs = puzzle.listPuzzles(pattern)
+                    val = pzs
+                    for a in pzs:
+                        print(a)
+                    return {'command': cmd, 'value': val}
+                case 'load' | 'open':
+                    if len(items) == 2:
+                        val = puzzle.choosePuzzle(items[1])
+                    elif len(items) == 3:
+                        val = puzzle.choosePuzzle(items[1], items[2])
+                    else:
+                        print("loading: ")
+                    return {'command':cmd, 'value':val}
+                case 'exit' | 'quit' | 'stop':
+                     sys.exit()
+                case _:
+                    print(f"unknown: {command}")
     #except:
         #We will have a better process later.  Right now, do this simply
         #print("something went wrong parsing.")
