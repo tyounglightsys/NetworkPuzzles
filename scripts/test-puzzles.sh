116579197ca9890650e2e70ec6f69121be4a23fc
tyounglightsys <tim.young@lightsys.org>
2025-06-09 10:26:03 -0700

    Updated the test_puzzles.sh script to allow parameters. You can test one puzzle, or one level: scripts/test-puzzles.sh level0 scripts/test-puzzles.sh ping scripts/test-puzzles.sh

--- scripts/test-puzzles.sh
+++ scripts/test-puzzles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-ls tests/solutions/* | while read filename; do
+
+function test_one_file {
+    filename=$1
     echo -n "testing $filename: "
     result=$(cat $filename | python -m src.network_puzzles | grep -i Congratulations)
     if [ -z "$result" ]; then
@@ -7,4 +9,24 @@ ls tests/solutions/* | while read filename; do
     else
         echo "OK"
     fi
-done
+}
+
+function expand_file {
+    totest="$1"
+    find tests/solutions | grep -i "$1"
+}
+
+if [ $# -eq 0 ]; then
+    ls tests/solutions/* | while read filename; do
+        test_one_file $filename
+    done
+else
+    #we have some command-line parameters.  Hopefully they are puzzle names
+    while [ $# -gt 0 ]; do
+        grabfiles=$(expand_file $1)
+        shift
+        for onefile in $grabfiles; do
+            test_one_file $onefile
+        done
+    done
+fi
