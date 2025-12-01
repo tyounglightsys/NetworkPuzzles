d7241f95a87a5af97298c733066e8b9f569b07f9
n8marti <nate_marti@sil.org>
2025-12-01 15:13:30 -0600

    Fix apk scrolling (#32)
    * chore: add missing colons
    
    * refactor: move New Items menu button to PuzzleLayout class
    
    * refactor: tray toggle logic moved to tray instances

--- src/network_puzzles/gui/layouts.py
+++ src/network_puzzles/gui/layouts.py
@@ -5,15 +5,79 @@ from kivy.metrics import sp
 from kivy.properties import NumericProperty
 from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
-from kivy.uix.relativelayout import RelativeLayout
 
 from .. import session
+from .base import NETWORK_ITEMS
 from .buttons import MenuButton
 
 
 class ThemedBoxLayout(BoxLayout):
+    pass
+
+
+class AppTray(ThemedBoxLayout):
+    def __init__(
+        self, choices=list(), orientation="horizontal", parent_button=None, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.app = session.app
+        self.parent_button = parent_button
+        self.choices = choices
+        self.buttons = [MenuButton(c) for c in self.choices]
+        self.orientation = orientation
+
+    @property
+    def is_open(self):
+        return len(self.children) > 0
+
+    def close(self):
+        self.clear_widgets()
+        self.app.root.ids.layout.remove_widget(self)
+
+    def open(self):
+        self.app.root.ids.layout.add_widget(self)
+        for b in self.buttons:
+            self.add_widget(b)
+        self._set_size()
+        self._set_pos()
+
+    def _set_pos(self):
+        if self.orientation == "horizontal":
+            # NOTE: The tray's pos is immediately adjacent to the anchor button.
+            x = self.parent_button.pos[0] + self.height
+            y = self.parent_button.pos[1]
+        else:
+            # NOTE: The tray's pos is offset down from the anchor button by the
+            # tray's height.
+            x = self.parent_button.pos[0]
+            y = self.parent_button.pos[1] - self.height
+        self.pos = (x, y)
+
+    def toggle(self):
+        if self.is_open:
+            self.close()
+        else:
+            self.open()
+
+    def _set_size(self):
+        length = len(self.children)
+        breadth = self.app.BUTTON_MAX_H + dp(10)  # 10px more for padding
+        if self.orientation == "horizontal":
+            self.width = length * breadth
+            self.height = breadth
+        else:
+            self.width = breadth
+            self.height = length * breadth
+
+
+class SelectableRecycleBoxLayout(
+    FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout
+):
+    """Adds selection and focus behaviour to the view."""
+
     pass
 
 
@@ -25,6 +89,37 @@ class PuzzleLayout(RelativeLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.app = session.app
+        self.items_menu_button = MenuButton(
+            props={"text": "+", "cb": self.app.on_new_item, "info": "add new item"},
+            pos_hint={"x": 0.005, "top": 0.99},
+        )
+
+        # Define button trays.
+        self.items_tray = AppTray(
+            parent_button=self.items_menu_button,
+            choices=self._get_items_choices(),
+            orientation="vertical",
+        )
+        self.infra_devices_tray = AppTray(
+            parent_button=self.items_tray.buttons[1],
+            choices=self._get_infra_devices_choices(),
+        )
+        self.user_devices_tray = AppTray(
+            parent_button=self.items_tray.buttons[2],
+            choices=self._get_user_devices_choices(),
+        )
+
+    def add_items_menu_button(self):
+        self.add_widget(self.items_menu_button)
+
+    def close_trays(self):
+        # Close tray and subtrays if open.
+        for tray in (
+            self.infra_devices_tray,
+            self.user_devices_tray,
+            self.items_tray,
+        ):
+            tray.close()
 
     def get_height(self):
         # Window height minus terminal area height.
@@ -56,52 +151,35 @@ class PuzzleLayout(RelativeLayout):
                     # child widgets (e.g. Links) are notified.
                     return super().on_touch_up(touch)
 
+    def _get_infra_devices_choices(self):
+        choices = []
+        for dtype, choice in NETWORK_ITEMS.get("devices").get("infrastructure").items():
+            choice["cb"] = self.app.add_device
+            choice["cb_kwargs"] = {"dtype": dtype}
+            choice["orientation"] = "horizontal"
+            choices.append(choice)
+        return choices
 
-class AppMenu(ThemedBoxLayout):
-    def __init__(
-        self, anchor_pos=None, choices=list(), orientation="horizontal", **kwargs
-    ):
-        super().__init__(**kwargs)
-        self.app = session.app
-        self.anchor_pos = anchor_pos  # parent_button.pos as (x, y)
-        self.choices = choices
-        self.orientation = orientation
+    def _get_items_choices(self):
+        return [
+            {"img": "link.png", "cb": self.app.add_link, "info": "add link"},
+            {
+                "img": "Switch.png",
+                "cb": self.app.on_new_infra_device,
+                "info": "infrastructure devices",
+            },
+            {
+                "img": "PC.png",
+                "cb": self.app.on_new_user_device,
+                "info": "user devices",
+            },
+        ]
 
-    def open(self):
-        for c in self.choices:
-            self.add_widget(MenuButton(c))
-        self._set_size()
-        self._set_pos()
-
-    def close(self):
-        self.clear_widgets()
-
-    def _set_pos(self):
-        if self.orientation == "horizontal":
-            # NOTE: The tray's pos is immediately adjacent to the anchor button.
-            x = self.anchor_pos[0] + self.height
-            y = self.anchor_pos[1]
-        else:
-            # NOTE: The tray's pos is offset down from the anchor button by the
-            # tray's height.
-            x = self.anchor_pos[0]
-            y = self.anchor_pos[1] - self.height
-        self.pos = (x, y)
-
-    def _set_size(self):
-        length = len(self.children)
-        breadth = self.app.BUTTON_MAX_H + dp(10)  # 10px more for padding
-        if self.orientation == "horizontal":
-            self.width = length * breadth
-            self.height = breadth
-        else:
-            self.width = breadth
-            self.height = length * breadth
-
-
-class SelectableRecycleBoxLayout(
-    FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout
-):
-    """Adds selection and focus behaviour to the view."""
-
-    pass
+    def _get_user_devices_choices(self):
+        choices = []
+        for dtype, choice in NETWORK_ITEMS.get("devices").get("user").items():
+            choice["cb"] = self.app.add_device
+            choice["cb_kwargs"] = {"dtype": dtype}
+            choice["orientation"] = "horizontal"
+            choices.append(choice)
+        return choices
