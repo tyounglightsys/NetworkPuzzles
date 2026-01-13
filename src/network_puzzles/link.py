e984ef65dfa3aa59604751bc4c1821ead4067e9b
Nate Marti <nate_marti@sil.org>
2026-01-13 06:46:30 -0600

    fix: general Link enhancement & cleanup

--- src/network_puzzles/link.py
+++ src/network_puzzles/link.py
@@ -2,30 +2,33 @@ class Link:
     def __init__(self, linkrec={}):
         self.json = linkrec
 
+    def __str__(self):
+        return self.hostname
+
     @property
-    def dest(self):
+    def dest(self) -> str:
         return self.json.get("DstNic").get("hostname", "")
 
     @property
     def dest_nic(self):
-        return self.json.get("DstNic", "")
+        return self.json.get("DstNic")
 
     @property
-    def hostname(self):
+    def hostname(self) -> str:
         return self.json.get("hostname", "")
 
     @property
-    def linktype(self):
+    def linktype(self) -> str:
         return self.json.get("linktype", "")
 
     @property
-    def src(self):
+    def src(self) -> str:
         return self.json.get("SrcNic").get("hostname", "")
 
     @property
     def src_nic(self):
-        return self.json.get("SrcNic", "")
+        return self.json.get("SrcNic")
 
     @property
-    def uniqueidentifier(self):
+    def uniqueidentifier(self) -> str:
         return self.json.get("uniqueidentifier", "")
