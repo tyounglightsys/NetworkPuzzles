f1a78be8ccd2fc3d698857a1c5c08978002aaf57
Nate Marti <nate_marti@sil.org>
2026-03-13 14:36:01 -0500

    feature: auto-select-all when focusing on TextInput

--- src/network_puzzles/gui/inputs.py
+++ src/network_puzzles/gui/inputs.py
@@ -1,5 +1,10 @@
+from kivy.clock import Clock
 from kivy.uix.textinput import TextInput
 
 
 class ValueInput(TextInput):
-    pass
+    def schedule_select_all(self):
+        # NOTE: The delay needs to be > 0 sec (next frame), otherwise
+        # the select_all action is overridden by default deselect_all.
+        delay = 0.1
+        Clock.schedule_once(lambda dt: self.select_all(), delay)
