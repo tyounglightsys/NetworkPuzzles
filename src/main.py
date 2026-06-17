65059b7065b665d9541e7971e37a2037ffaa1131
Nate Marti <nate_marti@sil.org>
2026-06-17 17:35:28 -0500

    modify main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 """Run the GUI App"""
 
+import os
 import sys
+
+# 1. FORCE KIVY TO USE A DUMMY WINDOW IN HEADLESS ENVIRONMENTS (CI/CD)
+# We check if we are running in GitHub Actions or if the dummy flag is passed
+if "GITHUB_ACTIONS" in os.environ or os.environ.get("KIVY_WINDOW") == "dummy":
+    os.environ["KIVY_NO_ARGS"] = "1"
+    os.environ["KIVY_WINDOW"] = "dummy"
+
 from network_puzzles.gui import NetworkPuzzlesApp
 from network_puzzles.ui import GUI
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         app = GUI(kivyapp=NetworkPuzzlesApp)
         app.run()
