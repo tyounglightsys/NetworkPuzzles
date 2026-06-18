68fb5e69474621e8ae46d1458e789a82b19ffaaf
Nate Marti <nate_marti@sil.org>
2026-06-18 07:32:06 -0500

    remove dummy window var

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 """Run the GUI App"""
 
-import os
+# import os
 import sys
 
-# 1. FORCE KIVY TO USE A DUMMY WINDOW IN HEADLESS ENVIRONMENTS (CI/CD)
-# We check if we are running in GitHub Actions or if the dummy flag is passed
-if "GITHUB_ACTIONS" in os.environ or os.environ.get("KIVY_WINDOW") == "dummy":
-    os.environ["KIVY_NO_ARGS"] = "1"
-    os.environ["KIVY_WINDOW"] = "dummy"
-
+# # 1. FORCE KIVY TO USE A DUMMY WINDOW IN HEADLESS ENVIRONMENTS (CI/CD)
+# # We check if we are running in GitHub Actions or if the dummy flag is passed
+# if "GITHUB_ACTIONS" in os.environ or os.environ.get("KIVY_WINDOW") == "dummy":
+#     os.environ["KIVY_NO_ARGS"] = "1"
+#     os.environ["KIVY_WINDOW"] = "dummy"
 from network_puzzles.gui import NetworkPuzzlesApp
 from network_puzzles.ui import GUI
 
