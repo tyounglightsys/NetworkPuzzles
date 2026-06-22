83cda4c411fcc77ec67d4f067e11340366866913
Nate Marti <nate_marti@sil.org>
2026-06-22 08:46:27 -0500

    set env var on windows

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ from network_puzzles.ui import GUI
 if __name__ == "__main__":
     if hasattr(sys, "_MEIPASS"):
         resource_add_path(os.path.join(sys._MEIPASS))
+        if sys.platform == "win32":
+            os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
     try:
         app = GUI(kivyapp=NetworkPuzzlesApp)
         app.run()
